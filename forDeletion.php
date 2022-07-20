d725fbbbb24c6ebbb07e01ddcb9ad20e8e465cb0
Michail Sitmalidis <boxm16@yahoo.com>
2022-07-20 22:07:36 +0300

    more fix ups

--- forDeletion.php
+++ forDeletion.php
@@ -1,12 +1,4 @@
-<?php
-require_once 'Controller/UploadController.php';
-require_once 'Controller/ItemController.php';
-require_once 'Model/Item.php';
-$uploadController = new UploadController();
-$items = $uploadController->saveUploadedDataIntoDatabase();
 
-$itemController = new ItemController();
-?>
 <!DOCTYPE html>
 
 <html>
@@ -21,19 +13,13 @@ $itemController = new ItemController();
         </style>
     </head>
     <body>
-        <table >
-            <?php
-            foreach ($items as $item) {
-                $itemController->addNewItem($item);
-                /*  echo "<tr>"
-                  . "<td>" . $item->getCodes()[0] . "</td>"
-                  . "<td>" . $item->getBarcodes()[0] . "</td>"
-                  . "<td>" . $item->getDescription() . "</td>"
-                  . "<td>" . $item->getPosition() . "</td>"
-                  . "</tr>"; */
-            }
-            echo "done";
-            ?>
-        </table>
+
+        <form action="servant.php" method="POST" >
+            <input type='text' name='barcode'>
+            <button type="submit">Submit</button>
+        </form>
+        <?php
+        ?>
+
     </body>
 </html>
