48c059f60255d89e7d8e1599d0965cd4beb15bd8
Michail Sitmalidis <boxm16@yahoo.com>
2022-07-23 23:18:50 +0300

    download page added

--- index.php
+++ index.php
@@ -9,4 +9,6 @@ echo "<br>";
 echo "<a href='upload.php'>Upload Items From Excel File</a>";
 echo "<br>";
 echo "<a href='itemFinder.php'>Find Item</a>";
+echo "<br>";
+echo "<a href='download.php'>DOWNLOAD</a>";
 ?>
