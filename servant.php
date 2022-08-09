36fd9090aad85f2bea4ce0aff0d1b6639f64e9e5
Michail Sitmalidis <boxm16@yahoo.com>
2022-08-10 02:12:48 +0300

    barcodes added

--- servant.php
+++ servant.php
@@ -5,7 +5,7 @@ $itemController = new ItemController();
 $barcode = $_POST["barcode"];
 $item = $itemController->getItemFromBarcode($barcode);
 $position = $item->getPosition();
-$output = $position."n\ALTERNATIVE CODES";
+$output = $position."\nALTERNATIVE CODES";
 $barcodes = $item->getBarcodes();
 foreach ($barcodes as $barcode) {
     $output.="\n".$barcode;
