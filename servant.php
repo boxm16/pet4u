cda7cb5bdfe0ce3220d420931444eb2739e7de2f
Michail Sitmalidis <boxm16@yahoo.com>
2022-08-10 02:55:09 +0300

    br

--- servant.php
+++ servant.php
@@ -5,7 +5,7 @@ $itemController = new ItemController();
 $barcode = $_POST["barcode"];
 $item = $itemController->getItemFromBarcode($barcode);
 $position = $item->getPosition();
-$output = $position."<br>ALTERNATIVE CODES<br>---------------";
+$output = $position."<br>---------------<br>ALTERNATIVE CODES<br>---------------";
 $barcodes = $item->getBarcodes();
 foreach ($barcodes as $barcode) {
     $output.="<br>".$barcode;
