db991e849e34d298107114889f5a40c376752b2b
Michail Sitmalidis <boxm16@yahoo.com>
2022-08-13 00:46:09 +0300

    allPositions added

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require_once 'Controller/TechManController.php';
 
-echo "<a href='addNewItem.php'>Add New Item</a>";
-echo "<br>";
-echo "<a href='items.php'>All Items</a>";
-echo "<br>";
+//echo "<a href='addNewItem.php'>Add New Item</a>";
+//echo "<br>";
+//echo "<a href='items.php'>All Items</a>";
+//echo "<br>";
 echo "<a href='upload.php'>Upload Items From Excel File</a>";
 echo "<br>";
 echo "<a href='itemFinder.php'>Find Item</a>";
