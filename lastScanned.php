569321b54ab7801eca35b623ae9e56ecb7fed008
Michail Sitmalidis <boxm16@yahoo.com>
2022-11-06 22:49:06 +0200

    last scanned barcode REMOVE button added

--- lastScanned.php
+++ lastScanned.php
@@ -6,8 +6,19 @@ if (isset($_POST["deleteLastScan"])) {
     $sql = "DELETE FROM notes";
     $connection->exec($sql);
 }
+if (isset($_GET["removeBarcode"])) {
+    $barcode = $_GET["removeBarcode"];
+    $sql = "DELETE  FROM notes  WHERE barcode='$barcode'";
+    try {
+        $result = $connection->exec($sql);
+    } catch (\PDOException $e) {
+        echo $e->getMessage() . " Error Code:";
+        echo $e->getCode() . "<br>";
+        exit;
+    }
+}
 
-$sql = "SELECT * FROM notes";
+$sql = "SELECT * FROM notes LEFT JOIN altercode ON notes.barcode=altercode.altercode LEFT JOIN item ON altercode.item_id=item.id;";
 try {
     $result = $connection->query($sql)->fetchAll();
 } catch (\PDOException $e) {
@@ -22,6 +33,14 @@ try {
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <style>
+            table, th, td {
+                border: 1px solid ;
+                border-collapse: collapse;
+                font-size: 20px;
+            }
+
+        </style>
     </head>
     <body>
         <a href="index.php">INDEX</a>
@@ -31,19 +50,27 @@ try {
             <center>   <button style="background-color: green" type="submit">REFRESH</button></center>
         </form>
         <hr>
-        <?php
-        if (!$result) {
-            
-        } else {
-            foreach ($result as $row) {
 
-                $barcode = $row["barcode"];
-                 $notes = $row["note"];
-                echo "<h1>" .$barcode."----".$notes."</h1>";
-                echo "<br>";
+        <table>
+            <?php
+            if (!$result) {
+                
+            } else {
+                foreach ($result as $row) {
+
+                    $barcode = $row["barcode"];
+                    $notes = $row["note"];
+                    $description = $row["description"];
+                    echo "<tr>";
+                    echo "<td><h2>" . $barcode . "</h2></td>";
+                    echo "<td><h2>" . $description . "</h2></td>";
+                    echo "<td><h2>" . $notes . "</h2></td>";
+                    echo "<td><h2><a href='lastScanned.php?removeBarcode=$barcode'>REMOVE</a></h2></td>";
+                    echo "</tr>";
+                }
             }
-        }
-        ?>
+            ?>
+        </table>
 
         <hr>
         <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
