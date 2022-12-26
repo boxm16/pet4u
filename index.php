3edbea674f297d1df17376a37e2716c648fa08ff
Michail Sitmalidis <boxm16@yahoo.com>
2022-12-26 14:26:10 +0200

    added download links

--- index.php
+++ index.php
@@ -3,6 +3,33 @@
 require_once 'Controller/TechManController.php';
 
 echo "<h1>";
+
+echo "<br>";
+echo "<a href='afmBarcode.php' target='_blank'>AFM BARCODE</a>";
+echo "<br>";
+echo "<br>";
+echo "<a href='http://ec2-54-74-154-166.eu-west-1.compute.amazonaws.com:8080/Pet4U/index.htm' target='_blank'>GO NEW SERVER</a>";
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+echo "<hr>";
+echo "<a href='https://apkfab.com/pet4u20/com.example.pet4u_20/apk?h=08b49631092b377b83abc718d8c8be1aadba93e48aef74e6a2a7165c330af593'>DOWNLOAD ANDROID APPLICATION</a>";
+echo "<br>";
+echo "<br>";
+echo "<img src='images/qr_1.png' style='width:200px;height:200px;' >";
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+echo "<a href='https://i.diawi.com/p2b5AJ'>DOWNLOAD MIRROR</a>";
+echo "<br>";
+echo "<br>";
+echo "<img src='images/qr_2.png' style='width:200px;height:200px;' >";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+
+exit;
 //echo "<a href='addNewItem.php'>Add New Item</a>";
 //echo "<br>";
 //echo "<a href='items.php'>All Items</a>";
@@ -14,8 +41,7 @@ echo "<a href='itemFinder.php'>Find Item</a>";
 //echo "<a href='download.php'>DOWNLOAD</a>";
 echo "<br>";
 echo "<a href='lastScanned.php'>LAST SCANNED BARCODES</a>";
-echo "<br>";
-echo "<a href='afmBarcode.php' target='_blank'>AFM BARCODE</a>";
+
 //echo "<br>";
 //echo "<a href='allPositions.php' target='_blank'>SHOW ALL ITEMS BY POSITION</a>";
 echo "<br>";
@@ -35,13 +61,7 @@ echo "<br>";
 echo "<a href='uploadSales.php'>Upload Sales</a>";
 echo "<hr>";
 echo "<br>";
-echo "<a href='https://apkfab.com/pet4u/com.example.pet4u/apk?h=97fa403d0c17cd428780c62eab2d30717e9595557af5559f857384ee765fe3bc'>DOWNLOAD ANDROID APPLICATION</a>";
-echo "<br>";
-echo "<hr>";
-echo "<a href='https://i.diawi.com/Meotdv'>DOWNLOAD MIRROR</a>";
-echo "<br>";
-echo "<hr>";
-echo "<a href='https://appsenjoy.com/rnRGD'>ANOTHER DOWNLOAD MIRROR</a>";
+
 
 echo "</h1>";
 ?>
